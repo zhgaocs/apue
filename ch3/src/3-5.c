f1e10346b091f67f28ae72914b1af28f4bcd056e
zhgaocs <93659392+zhgaocs@users.noreply.github.com>
2023-10-26 10:52:33 +0800

    Update 3-5.c
    fnctl ---> fcntl

--- ch3/src/3-5.c
+++ ch3/src/3-5.c
@@ -1,18 +1,44 @@
 /**
- * open one or more file status flags for a file descriptor
+ * open/close one or more file status flags for a file descriptor
  */
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void set_fl(int fd, int flags) // flags are file status flags to turn on
 {
     int val;
 
     if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+    {
         fprintf(stderr, "fcntl F_GETFL error");
+        exit(1);
+    }
 
     val |= flags; // turn on flags
 
-    if (fnctl(fd, F_SETFL, val) < 0)
+    if (fcntl(fd, F_SETFL, val) < 0)
+    {
         fprintf(stderr, "fcntl F_SETFL error");
+        exit(1);
+    }
+}
+
+void clr_fl(int fd, int flags) // flags are file status flags to turn off
+{
+    int val;
+
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+    {
+        fprintf(stderr, "fcntl F_GETFL error");
+        exit(1);
+    }
+
+    val &= ~flags; // turn flags off
+
+    if (fcntl(fd, F_SETFL, val) < 0)
+    {
+        fprintf(stderr, "fcntl F_SETFL error");
+        exit(1);
+    }
 }
