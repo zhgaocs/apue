52676343e650cf3e7b68027867c50365d2690f72
zhgaocs <93659392+zhgaocs@users.noreply.github.com>
2023-10-26 10:54:03 +0800

    Add files via upload

--- ch3/src/3-1.c
+++ ch3/src/3-1.c
@@ -1,1 +1,16 @@
+/**
+ * is it possible to set an offset to standard input
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main()
+{
+    if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
+        printf("cannot seek\n");
+    else
+        printf("seek ok\n");
+
+    exit(0);
+}
